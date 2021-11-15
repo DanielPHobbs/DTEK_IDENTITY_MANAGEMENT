1d6adbe58a57ab706b6dd2f6be2d522c9e0a54f6
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2021-11-15 15:48:01 +0000

    Update PSVAULT-SET-GET-HVAULT-SECRET.ps1

--- HASHICORP VAULT/POWERSHELL-SECRETS/PSVAULT-SET-GET-HVAULT-SECRET.ps1
+++ HASHICORP VAULT/POWERSHELL-SECRETS/PSVAULT-SET-GET-HVAULT-SECRET.ps1
@@ -20,7 +20,7 @@ get-help -name get-VaultSecret
 
 #Connect-Vault [[-VaultPath] <String>] [[-APIAddress] <Object>] [-token] <Object> [-quite] [<CommonParameters>]
 
-$vaultConnect=Connect-Vault -VaultPath  "dtek-SCO-KV" -APIAddress "http://HashiVault.dtek.com:8200" -token "s.u5eAjYu9tdRDkmrZe0L3TwBu"
+$vaultConnect=Connect-Vault -VaultPath  "dtek-SCO-KV" -APIAddress "http://10.10.10.75:8200" -token "s.u5eAjYu9tdRDkmrZe0L3TwBu"
 #ok
 
 $vaultstatus =  get-vaultstatus -apiaddress $APIaddress
@@ -28,7 +28,7 @@ $vaultstatus
 
 #get-Vaultobject [-Address] <String> [-Token] <String> [<CommonParameters>]
 
-$vaultobject= get-Vaultobject -Address "http://HashiVault.dtek.com:8200" -token "s.Z8KCd2sFSDJN1FtBREGy4g8t"
+$vaultobject= get-Vaultobject -Address "http://10.10.10.75:8200" -token "s.Z8KCd2sFSDJN1FtBREGy4g8t"
 $vaultobject
 #ok
 
